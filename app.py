58b8f3b6a5dff7bdf31fbaee0a909db1c67b483a
AyushSinghal1794 <89984761+AyushSinghal1794@users.noreply.github.com>
2022-07-09 01:40:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,12 +33,10 @@ ip12 = st.number_input('Enter the number of major vessels')
 op[11] = ip12
 ip13 = st.number_input('Enter the thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
 op[12] = ip13
-pred_var = model.predict([op])
+pred_var = model.predict(op)
 if st.button("Predict"):
   if pred_var == 1:
-    st.write(pred_var)
     st.write("Person has *Higher* Chance of Heart Disease")
   else:
-    st.write(pred_var)
     st.balloons()
     st.write("Person does not have chances of Heart Disease")
