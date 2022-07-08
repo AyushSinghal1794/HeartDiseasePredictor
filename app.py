fd00616d6c9f0bc616991d4d73ee8f9ee512ccff
AyushSinghal1794 <89984761+AyushSinghal1794@users.noreply.github.com>
2022-07-08 12:59:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,31 +7,31 @@ This App Predicts The Chances of a **Heart Disease** based on a Person's Medical
 """
 )
 op =[None]*13
-ip = st.text_input(int(float('Enter the Age')))
+ip = st.number_input('Enter the Age')
 op[0] = ip
-ip2 = st.text_input(int(float('Enter the sex')))
+ip2 = st.number_input('Enter the sex')
 op[1] = ip2
-ip3 = st.text_input(int(float('Enter the chest pain type')))
+ip3 = st.number_input('Enter the chest pain type')
 op[2] = ip3
-ip4 = st.text_input(int(float('Enter The resting blood pressure')))
+ip4 = st.number_input('Enter The resting blood pressure')
 op[3] = ip4
-ip5 = st.text_input(int(float('Enter the cholestrol level')))
+ip5 = st.number_input('Enter the cholestrol level')
 op[4] = ip5
-ip6 = st.text_input(int(float('Enter the fasting blood sugar')))
+ip6 = st.number_input('Enter the fasting blood sugar')
 op[5] = ip6
-ip7 = st.text_input(int(float('Enter the resting electrocardiographic results')))
+ip7 = st.number_input('Enter the resting electrocardiographic results')
 op[6] = ip7
-ip8 = st.text_input(int(float('Enter the Maximum heart rate')))
+ip8 = st.number_input('Enter the Maximum heart rate')
 op[7] = ip8
-ip9 = st.text_input(int(float('Enter the exercise induced angina')))
+ip9 = st.number_input('Enter the exercise induced angina')
 op[8] = ip9
-ip10 = st.text_input(int(float('Enter the oldpeak')))
+ip10 = st.number_input('Enter the oldpeak')
 op[9] = ip10
-ip11 = st.text_input(int(float('Enter the the slope of the peak exercise ST segment')))
+ip11 = st.number_input('Enter the the slope of the peak exercise ST segment')
 op[10] = ip11
-ip12 = st.text_input(int(float('Enter the number of major vessels')))
+ip12 = st.number_input('Enter the number of major vessels')
 op[11] = ip12
-ip13 = st.text_input(int(float('Enter the thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')))
+ip13 = st.number_input('Enter the thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
 op[12] = ip13
 OP = model.predict([op])
 if st.button('Predict'):
