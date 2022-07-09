5566ad923a4f87da79137f66797184021dec12c9
AyushSinghal1794 <89984761+AyushSinghal1794@users.noreply.github.com>
2022-07-09 09:35:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,12 +34,10 @@ ip12 = st.number_input('Enter the number of major vessels')
 op[11] = ip12
 ip13 = st.number_input('Enter the thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
 op[12] = ip13
-mo = open('HeartDiseasePrediction.pkl', 'rb')
-pred_var = pickle.load(mo)
-prediction = pred_var.predict([op])
-st.write(prediction)
+pred_var = model.predict([op])
+st.write(pred_var)
 if st.button("Predict"):
-  if prediction == 1:
+  if pred_var == 1:
     st.write("Person has *Higher* Chance of Heart Disease")
   else:
       st.balloons()
