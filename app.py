1512b76f74f2ff58c3bca80348c3cc39c28a2912
AyushSinghal1794 <89984761+AyushSinghal1794@users.noreply.github.com>
2022-10-12 18:37:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,6 +40,7 @@ pred_var = model.predict([op])
 if st.button("Predict"):
   if pred_var == 1:
     st.write("Person has **Higher** Chance of Heart Disease")
+    st.line_chart(op)
   else:
     st.balloons()
     st.write("Person has **Lower** Chance of Heart Disease")
